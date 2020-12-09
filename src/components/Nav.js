95f22b8ecfa80674bbabc30f35ab2354555944e2
Joey <joey7040@hotmail.com>
2020-12-09 18:12:31 -0500

    secondary nav with animations now added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,18 @@ import '../css/Nav.css';
 
 function Nav() {
     return (
-        <div className='Nav'>
-            <h1>Never Give Up!</h1>
+        <div className='nav'>
+            <h2>Trending</h2>
+            <h2>Top Rated</h2>
+            <h2>Action</h2>
+            <h2>Comedy</h2>
+            <h2>Horror</h2>
+            <h2>Romance</h2>
+            <h2>Mystery</h2>
+            <h2>Sci-fi</h2>
+            <h2>Western</h2>
+            <h2>Animation</h2>
+            <h2>Movie</h2>
         </div>
     )
 }
